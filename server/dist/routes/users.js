33814214284a47432c4df306c6a3e45aea7a61e3
CJ <cjfritz9@gmail.com>
2023-08-19 00:58:45 -0600

    added users router with create, read, update methods

--- server/dist/routes/users.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { httpGetUser, httpAddUser, httpUpdateUser } from '../controllers/users.controller';
-const usersRouter = express.Router();
-usersRouter.get('/:id', httpGetUser);
-usersRouter.post('/', httpAddUser);
-usersRouter.patch('/', httpUpdateUser);
